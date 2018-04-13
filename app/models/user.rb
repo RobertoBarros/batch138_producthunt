801abdba53246bd405a2e1d775430eeb280c2b0d
Roberto Barros <roberto.f.barros@gmail.com>
2018-04-13 09:59:21 -0300

    refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-
   def full_name
     "#{first_name.capitalize} #{last_name.capitalize}"
   end
